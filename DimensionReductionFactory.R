e2240780cd2982449c957b5e88da189de705aee2
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-11-21 11:15:11 +0100

    Add dimension reduction class

---
+++ DimensionReductionFactory.R
@@ -0,0 +1,33 @@
+library(methods)
+library(vegan)
+library(Rtsne)
+
+dimension_reduction_factory <- setRefClass("DimensionReductionFactory",
+                                           fields = list(
+                                              distributional_coords="matrix",
+                                              distance_matrix="matrix"),
+                                           methods = list(
+                                              # constructor
+                                              initialize = function(distributional_coords) {
+                                                distributional_coords <<- distributional_coords
+                                                distance_matrix <<- get_distance_matrix()
+                                              },
+                                              
+                                              get_distance_matrix = function() {
+                                                distance_matrix_ <- dist(distributional_coords, method="euclidean", diag=T, upper=T)
+                                                distance_matrix_ <- as.matrix(distance_matrix_)
+                                                return(distance_matrix_)
+                                              },
+                                              
+                                              do_mds = function() {
+                                                coordinates <- metaMDS(distance_matrix, k=2, parallel=numCores)$points
+                                                
+                                                return(coordinates)
+                                              },
+                                              
+                                              do_tsne = function() {
+                                                coordinates <- Rtsne(distance_matrix, is_distance=TRUE)$Y
+                                                
+                                                return(coordinates)
+                                              }
+))
